dea23957579532ab96304a9fe06b914d5ba44a6a
VictorXiao <843731025@qq.com>
2016-09-09 16:10:19 +0800

    Initial commit

---
+++ app/src/main/java/com/victor_xiao/intentfilteradvanced/MainActivity.java
@@ -0,0 +1,27 @@
+package com.victor_xiao.intentfilteradvanced;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setData(Uri.parse("course://www.hbs.xyz:8080/android"));
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/victor_xiao/intentfilteradvanced/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.victor_xiao.intentfilteradvanced;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Victor_Xiao on 16/09/05.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
